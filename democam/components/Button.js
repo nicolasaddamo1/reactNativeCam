0706a2affadaab4cf98e22a94a645324ee6949a7
Nico <nicolasaddamo1@gmail.com>
2025-01-29 16:46:45 -0300

    added styles

--- democam/components/Button.js
+++ democam/components/Button.js
@@ -1,4 +1,26 @@
 import * as React from 'react';
-export default Button = ({}) => {
-
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {MaterialIcons} from '@expo/vector-icons';
+export default Button = ({icon,size,color,style, onPress}) => {
+    return (
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+            <MaterialIcons name={icon}
+            size={size? size : 24 }
+            color={color} />
+        </TouchableOpacity>
+    ); 
 }
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 10,
+        backgroundColor: 'blue',
+        color: 'white',
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+});
